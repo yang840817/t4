eef9353a62be610fdf8be526c41f07d746e40f21
JoRne <frostyjoan0829@livemailtw>
2015-10-20 17:20:09 +0800

    IHsuan_t4

--- jorne_template4/config.rb
+++ jorne_template4/config.rb
@@ -5,7 +5,7 @@ Encoding.default_external = Encoding.find('utf-8')
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
-sass_dir = "sass"
+sass_dir = "scss"
 images_dir = "img"
 javascripts_dir = "js"
 
